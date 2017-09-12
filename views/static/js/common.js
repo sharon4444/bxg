e448d6ee076c4cfcc50058405d9aa6b3d70d47ce
pipixia <pipixia.com>
2017-09-12 19:31:48 +0800

    m退出和导航功能完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,12 +1,4 @@
 
-	// NProgress.start();
-
-	// NProgress.done();
-
-	// $('.navs ul').prev('a').on('click', function () {
-	// 	$(this).next().slideToggle();
-	// });
-
   define(["jquery", "template", "cookie"], function($, template){
   $(function(){
    
@@ -19,5 +11,35 @@
       var html = template("profile_tpl", userinfo);
       $("#profile").html(html);
     }
+
+    // 头部退出功能
+    $("#logout-btn").click(function(){
+      console.log(1)
+      $.ajax({
+        url:"/api/logout",
+        type:"post",
+        success:function(data){
+          if(data.code == 200){
+            location.href = "/dashboard/login"
+          }
+        }
+      })
+    })
+
+    //实现子菜单栏的展示和隐藏
+    $("#nav-course").click(function(){
+      $(this).find("ul").stop().slideToggle();
+    })
+
+   //当前页面导航高亮
+    var navi =$(".navs>ul>li>a");
+    navi.each(function(i,e){
+      var _this = $(e);
+      if(_this.attr("href")== location.pathname){
+        _this.addClass("active")
+      }
+    })
+
   })  
 })
+
